ec66530f399ab2ebc67d0ffb246050a7d5a002e5
dytonpictures <73316169+dytonpictures@users.noreply.github.com>
2023-11-08 20:29:50 +0100

    Add files via upload

---
+++ windows/robert pc.ps1
@@ -0,0 +1,8 @@
+cscript slmgr.vbs /skms zh.us.to/ /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX /ato;
+winget install -e --id Discord.Discord;
+winget install -e --id Valve.Steam;
+winget install -e --id EpicGames.EpicGamesLauncher;
+winget install -e --id OBSProject.OBSStudio
+winget install -e --id Opera.Opera;
+winget install -e --id Microsoft.Teams;
+winget install -e --id Microsoft.WindowsTerminal;
